2d237b2c43acc401c01e67b167bdfc4ab1ec0295
lebrowley <brocious123@gmail.com>
2020-05-25 08:27:27 -0600

    data update

--- server/controllers/newBook.js
+++ server/controllers/newBook.js
@@ -11,11 +11,14 @@ module.exports = {
 
             if(id === index){
                 return newBook.push(book)
+            } else if(id >= 15){
+                return newBook.push(data[15])
+
             }
         })
         id++
         res.status(200).send(newBook)
-    }
+    }  
 }
 
-//what functionality could I add so it won't crash at the end of the book data array?
+//is there a better way of handling the end of data-- in the real world with an api, would there just be so much data that you wouldn't have to worry about what to do if your data ran out?? or are there ways (better than what I did above) to make it so you can smoothly tell your users there isn't any more data to display??
